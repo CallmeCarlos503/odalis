de1c62b4356c697abfda0deb6f5255037b009e47
CallmeN <95440504+CallmeCarlos503@users.noreply.github.com>
2023-04-28 17:48:06 -0600

    finalizado

---
+++ ejercicio_for/ejercicio_for/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int acumulador=0; //Acumulador de 12 meses
+                          
+        for(int i = 1; i <=15; i++) {
+            acumulador += 12;
+            Console.WriteLine("año " + i +": "+acumulador);
+        }
+
+        Console.ReadKey();
+    }
+}
