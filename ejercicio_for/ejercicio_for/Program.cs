c1aae12ec19bae41b491cc89906ec9f7cbcf6519
CallmeN <95440504+CallmeCarlos503@users.noreply.github.com>
2023-04-28 17:49:52 -0600

    finalizado

--- ejercicio_for/ejercicio_for/Program.cs
+++ ejercicio_for/ejercicio_for/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
@@ -8,10 +8,11 @@ class Program
         int acumulador=0; //Acumulador de 12 meses
                           
         for(int i = 1; i <=15; i++) {
-            acumulador += 12;
-            Console.WriteLine("año " + i +": "+acumulador);
+            acumulador += 12; //acumulador sumara el 12 alrededor de 15 pasos del for, += se utiliza de manera abreviada
+
+            Console.WriteLine("año " + i +": "+acumulador);  //escribe el resultado final
         }
 
-        Console.ReadKey();
+        Console.ReadKey();  //Bloquea la salida del cmd
     }
 }
